92de375633c9afb14ce85b04e82c235bf449232c
Renad <ren4dali@gmail.com>
2024-03-09 14:18:15 +0300

    revert commit.

--- app/src/main/java/com/renad/demoforlist/data/source/remote/RecipeApiService.kt
+++ app/src/main/java/com/renad/demoforlist/data/source/remote/RecipeApiService.kt
@@ -5,13 +5,14 @@ import com.renad.demoforlist.data.model.RecipesModel
 import retrofit2.http.GET
 import retrofit2.http.Path
 
+
 interface RecipeApiService {
     // Get Random list of Recipes.
-    @GET("/recipes/random?number=10&apiKey=${""}")
+    @GET("/recipes/random?number=10&apiKey=${BuildConfig.API_KEY}")
     suspend fun getRandomRecipes(): RecipesModel
 
     // Get Recipe by id.
-    @GET("/recipes/{id}/information?includeNutrition=false&apiKey=${""}")
+    @GET("/recipes/{id}/information?includeNutrition=false&apiKey=${BuildConfig.API_KEY}")
     suspend fun getRecipe(
         @Path("id") id: String,
     ): RecipeModel
